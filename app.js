833467f9d383f45604058fb6b29f256e457bdd28
mohikaner <mohikaner@quantentunnel.de>
2014-06-04 00:10:38 +0200

    first attempt to get hasMany association to work

--- app.js
+++ app.js
@@ -5,30 +5,45 @@ Deepr.Project = DS.Model.extend({
     name: DS.attr('string'),
     url: DS.attr('string'),
     imageurl: DS.attr('string'),
-    timebudget: DS.attr(),
-    timelogs: DS.hasMany('timelog')
+    timebudget: DS.attr('number'),
+    timelogs: DS.hasMany('timelog', {async: true}),
+    status: DS.attr('string'),
+    
+    total: function(){
+	
+    }.property('timebudget', 'timelogs')
 });
 
 Deepr.Person = DS.Model.extend({
     firstname: DS.attr('string'),
     lastname: DS.attr('string'),
     imageurl: DS.attr('string'),
-    timelogs: DS.hasMany('timelog')
+    timelogs: DS.hasMany('timelog', {async: true})
 });
 
 Deepr.Timelog = DS.Model.extend({
     date: DS.attr('date'),
     comment: DS.attr('string'),
-    hours: DS.attr()
+    hours: DS.attr('number')
 });
 
 // Fixture data
 Deepr.Project.FIXTURES= [
-    {id: 1, name: "Yaez", url: "www.yaez.de", imageurl: "www.yaez.de/logo.png", timebudget: 50, timelogs: [] },
-    {id: 2, name: "Carbowheel", url: "www.carbowheel.de", imageurl: "www.carbowheel.de/logo.png", timebudget: 50, timelogs: [] },
-    {id: 3, name: "Transferre", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [] },
-    {id: 4, name: "Montagetools", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [] },
-    {id: 5, name: "cc-bs", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [] },
+    {id: 1, name: "Yaez", url: "www.yaez.de", imageurl: "www.yaez.de/logo.png", timebudget: 50, timelogs: [1,2,3], status: 'open' },
+    {id: 2, name: "Carbowheel", url: "www.carbowheel.de", imageurl: "www.carbowheel.de/logo.png", timebudget: 50, timelogs: [4,5,6], status: 'open' },
+    {id: 3, name: "Transferre", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [7], status: 'closed' },
+    {id: 4, name: "Montagetools", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [], status: 'closed' },
+    {id: 5, name: "cc-bs", url: "www.transferre.de", imageurl: "www.transferre.de/logo.png", timebudget: 35, timelogs: [], status: 'open' },
+]
+
+Deepr.Timelog.FIXTURES = [
+    {id: 1, comment: "Super gemacht", hours: 3},
+    {id: 2, comment: "Super toll gemacht", hours: 4},
+    {id: 3, comment: "Nicht ganz fertig", hours: 5},
+    {id: 4, comment: "Blubb", hours: 2.5},
+    {id: 5, comment: "Ninjas rockens mal wieder", hours: 1},
+    {id: 6, comment: "Pirates ftw", hours: 3.5},
+    {id: 7, comment: "AO", hours: 2}
 ]
 
 Deepr.Router.map(function (){
